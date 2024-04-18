4a3b6360a6cff6fe61aeb9296dea71fe9e1c36c3
Masooma <masoomashah05@gmail.com>
2024-04-18 13:55:59 +0500

    signup

--- new.cpp
+++ new.cpp
@@ -1,5 +1,49 @@
 #include <iostream>
+#include <fstream>
+
 using namespace std;
+struct DOB
+{
+int date;
+int month;
+int year;
+};
+class Person
+{
+    protected:
+    string username;
+    string password;
+    string email;
+    string country;
+    DOB dob; 
+    
+    public:
+
+    void signup()
+    {ifstream in;
+    ofstream out;
+    cout<<""
+       cout<<"Enter your Username"<<endl;
+       cin>>username;
+
+
+    }
+    
+};
+class User:public Person
+{private:
+int bids=30;
+public:
+User()
+{
+
+}
+};
+class Employer:public Person
+{
+string oragnization;
+
+};
  int main()
  { 
 
